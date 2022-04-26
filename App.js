01f7744fff1497769bc718d7a437eb7e2798102b
haillie <hsj30338@gmail.com>
2022-04-25 18:43:34 -0700

    Update Date coverting toString() to toUTCString(), npm and expo go version

--- App.js
+++ App.js
@@ -5,10 +5,10 @@ import { Fontisto } from "@expo/vector-icons";
 import { StyleSheet, Text, View, ScrollView, Dimensions, ActivityIndicator } from 'react-native';
 
 const { width:SCREEN_WIDTH } = Dimensions.get("window");
-// const SCREEN_WIDTH = Dimensions.get("window").width; // 위 코드와 같은 코드
+// const SCREEN_WIDTH = Dimensions.get("window").width; // same code with line 7
 // console.log(SCREEN_WIDTH); // print wdith of the device
 
-const API_KEY = "c6da87942b0e49491f5611e96492f5f9"; // 따로 서버가 없기에 일단 여기에 씀
+const API_KEY = "c6da87942b0e49491f5611e96492f5f9"; // this should be in a server, app suppose to send a request to the server
 
 const icons = {
   Clouds: "cloudy",
@@ -23,18 +23,18 @@ const icons = {
 export default function App() {
 
   const [city, setCity] = useState("Loading...");
-  // const [location, setLocation] = useState(null); //필요없음
-  const [days, setDays] = useState([]); // create an array for storing the forcasts
+  // const [location, setLocation] = useState(null); // no need to save location
+  const [days, setDays] = useState([]); // create an array for storing the forecasts
   const [check, setCheck] = useState(true);
 
   //ask a permission
-  // -> we are not asking the user's location, what we need is weather frocasts
+  // -> we are not asking the user's location, what we need is weather forecasts
   //    so, change the name ask to getWeather
   const getWeather = async() => {
     const { granted } = await Location.requestForegroundPermissionsAsync();
     // console.log(permission);
 
-    // 1. if the user rejected a location permission, when the location was not granted,
+    // 1. if the user rejected a location permission, and the location was not granted,
     // change the setCheck will pass false value
     if(!granted){
       setCheck(false);
@@ -59,7 +59,7 @@ export default function App() {
     // Replace the values from step 3
     const response = await fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=alerts&appid=${API_KEY}&units=metric`);
     const json = await response.json();
-    // console.log(json);
+    console.log(json);
     setDays(json.daily);
     // console.log(json.daily);
   }; 
@@ -94,7 +94,7 @@ export default function App() {
                       size={ 70 } 
                       color="#EEA47F" />
                   </View>
-                  <Text style={styles.date}> { new Date(day.dt*1000).toString().substring(0, 10) } </Text>
+                  <Text style={styles.date}> { new Date(day.dt*1000).toUTCString().substring(0, 16) } </Text>
                   <Text style={styles.description}>{day.weather[0].main}</Text>
                   <Text style={styles.detailDes}>{day.weather[0].description}</Text>
                 </View>
@@ -107,47 +107,47 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container:{
-    flex: 1, 
+    flex           : 1,
     backgroundColor: "#00539C"
   },
   city: {
-    flex: 1.1,
+    flex          : 1.1,
     justifyContent: "center",
-    alignItems: "center"
+    alignItems    : "center"
   },
   cityName: {
-    fontSize: 48,
-    color: "#EEA47F",
+    fontSize  : 48,
+    color     : "#EEA47F",
     fontWeight: "400",
   },
   date: {
-    fontSize: 30,
-    color: "#EEA47F",
-    fontWeight: "400",
+    fontSize   : 30,
+    color      : "#EEA47F",
+    fontWeight : "400",
     marginStart: -5,
   },
   day: {
-    width: SCREEN_WIDTH,
-    alignItems: "center",
-    alignItems: "flex-start",
+    width            : SCREEN_WIDTH,
+    alignItems       : "center",
+    alignItems       : "flex-start",
     paddingHorizontal: 30,
   },
   temp: {
-    marginTop: 50,
-    fontSize: 130,
+    marginTop : 50,
+    fontSize  : 130,
     fontWeight: "400",
-    color: "#EEA47F",
+    color     : "#EEA47F",
   },
   description: {
-    marginTop: -10,
-    fontSize: 60,
+    marginTop : -10,
+    fontSize  : 60,
     fontWeight: "300",
-    color: "#EEA47F",
+    color     : "#EEA47F",
   },
   detailDes: {
-    marginTop: -10,
-    fontSize: 30,
-    color: "#EEA47F",
+    marginTop : -10,
+    fontSize  : 30,
+    color     : "#EEA47F",
     fontWeight: "500",
   }
 })
--- README.md
+++ README.md
@@ -24,6 +24,24 @@
 - Making a diffrent layout with View and Scrollview tag
 
 
+## Challenge part
+- I wanted to put the date, but the format was, for example,
+
+        "dt": 1650949200
+
+I need to convert this to date format. Refer to this [link](https://www.w3schools.com/js/js_dates.asp) and this [article](https://javascript.tutorialink.com/how-to-get-data-info-from-openweathermap-api-dt/) I learned that numbers are milliseconds of the date of the day. For displaying the date coded like this:
+
+        new Date(day.dt*1000).toDateString() // display: Mon Apr 25 2022
+
+if substring() added
+
+        new Date(day.dt*1000).toDateString().substring(0, 10) // display: Mon Apr 25
+
+        new Date(day.dt*1000).toISOString().substring(0, 10) // display: 2022-04-25
+
+        new Date(day.dt*1000).toUTCString().substring(0, 16) // display: Mon, 25 Apr 2022
+
+
 
 ## Demo
 
